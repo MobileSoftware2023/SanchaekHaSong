60806cf45cc549b336f794fd9d505b03a661b256
seio01 <101907921+seio01@users.noreply.github.com>
2023-12-02 11:01:44 +0900

    update json

--- app/src/main/java/com/example/sanchaekhasong/login/LoginActivity.kt
+++ app/src/main/java/com/example/sanchaekhasong/login/LoginActivity.kt
@@ -11,7 +11,10 @@ import com.example.sanchaekhasong.MainActivity
 import com.example.sanchaekhasong.databinding.ActivityLoginBinding
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
@@ -72,32 +75,19 @@ class LoginActivity : AppCompatActivity() {
                     }
                 }
         }
+
+
     }
 
     private fun createAccount(email: String, password: String, profileImage:String, college : String) {
         if(isSchoolDomain(email)){
             auth = FirebaseAuth.getInstance()
-            auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this){task ->
-                    binding.emailText.text.clear()
-                    binding.passwordText.text.clear()
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this){task ->
                     if(task.isSuccessful){
-                        auth.currentUser?.sendEmailVerification()
-                            ?.addOnCompleteListener{ sendTask ->
+                        auth.currentUser?.sendEmailVerification()?.addOnCompleteListener{ sendTask ->
                                 if(sendTask.isSuccessful){
-                                    //프로필사진(png), username, 단과대 db에 저장
-                                    username = email.substringBeforeLast('@')
-                                    val database = FirebaseDatabase.getInstance()
-                                    val myData = database.getReference("$username")
-                                    myData.child("profileImage").setValue("$profileImage")
-                                    myData.child("college").setValue("$college")
-                                    myData.child("point").setValue(0)
-                                    val initProfileImage = listOf<String>("snow1", "snow2", "snow3", "snow4", "snow5")
-                                    myData.child("profileImageList").setValue(initProfileImage)
-                                    myData.child("boughtCouponList").setValue(true)
-                                    myData.child("boughtProfileImageList").setValue(true)
-                                    Toast.makeText(this, "회원가입 성공, 전송된 메일을 확인해 주세요",
-                                        Toast.LENGTH_SHORT).show()
+                                    setData(email, college, profileImage)
+                                    Toast.makeText(this, "회원가입 성공, 전송된 메일을 확인해 주세요", Toast.LENGTH_SHORT).show()
                                 }else {
                                     Toast.makeText(this, "메일 발송 실패", Toast.LENGTH_SHORT).show()
                                 }
@@ -112,6 +102,48 @@ class LoginActivity : AppCompatActivity() {
         {
             Toast.makeText(this, "회원가입 실패, 숙명계정으로 회원가입 해주세요.", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun setData(email: String, college: String, profileImage: String){
+        //username -> 프로필사진, 단과대, 포인트, 보유프로필사진(기본 + 구매), 구매한프로필사진, 구매한쿠폰, 걸음수총합
+        //@college -> 단과대 학생수 + 1
+        username = email.substringBeforeLast('@')
+        val database = FirebaseDatabase.getInstance()
+        val myData = database.getReference("$username")
+        myData.child("profileImage").setValue("$profileImage")
+        myData.child("college").setValue("$college")
+        myData.child("point").setValue(0)
+        val initProfileImage = listOf<String>("snow1", "snow2", "snow3", "snow4", "snow5")
+        myData.child("profileImageList").setValue(initProfileImage)
+        myData.child("boughtCouponList").setValue(true)
+        myData.child("boughtProfileImageList").setValue(true)
+        myData.child("sumWalkCount").setValue(0)
+        val challengeMissionList = listOf<String>(
+            "6,000 걸음 이상 걷기를 30회 달성", "8,000 걸음 이상 걷기를 30회 달성",
+            "10,000 걸음 이상 걷기를 30회 달성", "ㅇㅇ 루트 걷기를 30회 달성",
+            "ㅁㅁ 루트 걷기를 30회 달성", "ㄹㄹ 루트 걷기를 30회 달성"
+        )
+        myData.child("challenge").child("mission").setValue(challengeMissionList)
+        val progressList = listOf<Int>(0, 0, 0, 0, 0, 0)
+        myData.child("challenge").child("progress").setValue(progressList)
+        val dailyQuestList = listOf<String>("6,000 걸음 이상 걸어요.", "8,000 걸음 이상 걸어요.",
+            "10,000 걸음 이상 걸어요.", "ㅇㅇ 루트를 1회 걸어요.",
+            "ㅁㅁ 루트를 1회 걸어요.", "ㄹㄹ 루트를 1회 걸어요."
+        )
+        //단과대 학생수 등록
+        val myData1 = database.getReference("@college").child("$college")
+        myData1.addListenerForSingleValueEvent(object :
+            ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val currentValue = dataSnapshot.value as Int
+                val newValue = currentValue + 1
+                myData1.setValue(newValue)
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                // 처리 중 오류 발생 시 처리
+                Log.e("TAG_DB", "onCancelled", databaseError.toException())
+            }
+        })
     }
 
     private fun isSchoolDomain(email : String? ): Boolean {
